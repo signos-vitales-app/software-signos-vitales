bd1f270fc58cf937390f222049f45930d73b1cda
signos-vitales-app <Signosvitales.la2@gmail.com>
2024-11-21 18:20:38 -0500

    base de datos

--- basedatoscompleta.sql
+++ basedatoscompleta.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-11-2024 a las 06:21:34
+-- Tiempo de generación: 22-11-2024 a las 00:19:27
 -- Versión del servidor: 8.0.40
 -- Versión de PHP: 8.2.12
 
@@ -100,7 +100,7 @@ INSERT INTO `patients` (`id`, `primer_nombre`, `segundo_nombre`, `primer_apellid
 (51, 'José', 'Luis', 'Martínez', 'González', '78451392', 'tarjeta de identidad', '152A', 'activo', '2024-02-21 10:00:00', '2006-12-15', 'Recién nacido'),
 (52, 'Marcela', 'Andrea', 'Rodríguez', 'Gómez', '258963752', 'tarjeta de identidad', '153B', 'activo', '2024-02-22 10:00:00', '2007-07-19', 'Recién nacido'),
 (53, 'Tomás', 'Antonio', 'Gómez', 'Martínez', '123574689', 'cédula de ciudadanía', '154C', 'activo', '2024-02-23 10:00:00', '1996-05-12', ''),
-(54, 'Ana', 'Isabel', 'Sánchez', 'González', '963852748', 'cédula de ciudadanía', '155A', 'activo', '2024-02-24 10:00:00', '2024-11-20', 'Recién nacido'),
+(54, 'Ana', 'Isabel', 'Sánchez', 'González', '963852748', 'cédula de ciudadanía', '155A', 'activo', '2024-02-24 10:00:00', '2024-11-21', 'Recién nacido'),
 (55, 'Fernando', 'Gabriel', 'Martínez', 'Gómez', '258741562', 'cédula de ciudadanía', '156B', 'activo', '2024-02-25 10:00:00', '1987-03-23', ''),
 (56, 'Raúl', 'José', 'Sánchez', 'Martínez', '7458623', 'cédula de ciudadanía', '157C', 'activo', '2024-02-26 10:00:00', '1992-02-06', ''),
 (57, 'Marta', 'Patricia', 'Rodríguez', 'Fernández', '7456921', 'cédula de ciudadanía', '158A', 'activo', '2024-02-27 10:00:00', '2004-06-18', 'Recién nacido'),
@@ -124,6 +124,103 @@ INSERT INTO `patients` (`id`, `primer_nombre`, `segundo_nombre`, `primer_apellid
 (1079051, 'aesfdgf', 'sadb', 'szdxc', 'asfgf', '4865123152', 'cédula de ciudadanía', '562', 'activo', '2024-11-21 03:01:46', '2002-12-26', 'Adulto'),
 (1079052, 'eweqw', 'qeqwe', 'eqweqwe', 'qweqewqwe', '465123', 'cédula de ciudadanía', '12', 'activo', '2024-11-21 03:16:03', '2017-01-19', 'Preescolar tardío');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registros_paciente`
+--
+
+CREATE TABLE `registros_paciente` (
+  `id` int NOT NULL,
+  `id_paciente` int NOT NULL,
+  `record_date` date NOT NULL,
+  `record_time` time NOT NULL,
+  `presion_sistolica` int DEFAULT NULL,
+  `presion_diastolica` int DEFAULT NULL,
+  `presion_media` decimal(5,2) DEFAULT NULL,
+  `pulso` int DEFAULT NULL,
+  `temperatura` decimal(4,1) DEFAULT NULL,
+  `frecuencia_respiratoria` int DEFAULT NULL,
+  `saturacion_oxigeno` int DEFAULT NULL,
+  `peso_adulto` decimal(6,3) DEFAULT NULL,
+  `peso_pediatrico` decimal(4,1) DEFAULT NULL,
+  `talla` int DEFAULT NULL,
+  `observaciones` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `registros_paciente`
+--
+
+INSERT INTO `registros_paciente` (`id`, `id_paciente`, `record_date`, `record_time`, `presion_sistolica`, `presion_diastolica`, `presion_media`, `pulso`, `temperatura`, `frecuencia_respiratoria`, `saturacion_oxigeno`, `peso_adulto`, `peso_pediatrico`, `talla`, `observaciones`, `created_at`) VALUES
+(13, 1, '2024-11-16', '18:54:00', 80, 120, 83.00, 70, 35.0, 25, 100, NULL, 25.0, 150, '', '2024-11-16 23:55:35'),
+(14, 3, '2024-11-16', '18:57:00', 80, 120, 83.00, 60, 36.0, 70, 100, 45.000, NULL, 120, '', '2024-11-16 23:57:43'),
+(15, 1, '2024-11-17', '19:08:00', 80, 120, 83.00, 75, 35.0, 30, 100, 45.000, NULL, 120, '', '2024-11-17 00:08:33'),
+(16, 43, '2024-11-17', '23:42:00', 80, 120, 83.00, 70, 35.0, 70, 100, NULL, 45.0, 150, '', '2024-11-17 04:42:57'),
+(17, 47, '2024-11-17', '00:19:00', 110, 78, 83.00, 70, 35.0, 50, 100, NULL, 51.0, 150, '', '2024-11-17 05:20:09'),
+(18, 47, '2024-11-17', '00:44:00', 120, 80, 55.00, 55, 35.0, 11, 90, NULL, 45.0, 150, '', '2024-11-17 05:48:41'),
+(19, 47, '2024-11-17', '00:55:00', 120, 80, 83.00, 70, 36.5, 20, 100, NULL, 50.0, 150, '', '2024-11-17 05:57:15'),
+(20, 58, '2024-11-18', '07:54:00', 80, 120, 83.00, 80, 35.0, 30, 100, 48.000, NULL, 150, '', '2024-11-18 12:54:26'),
+(21, 1, '2024-11-18', '08:12:00', 80, 120, 83.00, 70, 36.0, 30, 100, 54.000, NULL, 120, '', '2024-11-18 13:13:31'),
+(22, 1, '2024-11-18', '08:49:00', 80, 120, 83.00, 50, 30.0, 50, 100, 86.000, NULL, 150, '', '2024-11-18 13:50:21'),
+(23, 31, '2024-11-18', '08:51:00', 80, 120, 83.00, 40, 34.0, 50, 100, 50.000, NULL, 120, '', '2024-11-18 13:51:46'),
+(24, 1, '2024-11-18', '09:06:00', 80, 120, 83.00, 80, 25.0, 60, 100, 50.000, NULL, 120, '', '2024-11-18 14:07:43'),
+(25, 1, '2024-11-18', '09:35:00', 80, 120, 83.00, 50, 35.0, 50, 100, 50.000, NULL, 150, '', '2024-11-18 14:35:45'),
+(26, 1, '2024-11-18', '09:50:00', 80, 120, 83.00, 50, 36.0, 50, 100, 50.000, NULL, 150, '', '2024-11-18 14:51:51'),
+(27, 1079047, '2024-11-18', '11:44:00', 100, 90, 83.00, 80, 35.0, 30, 100, 56.000, NULL, 150, '', '2024-11-18 16:44:53'),
+(28, 3, '2024-11-19', '21:48:00', 80, 120, 83.00, 50, 35.0, 30, 100, 45.000, NULL, 150, '', '2024-11-19 02:49:15'),
+(29, 47, '2024-11-19', '17:06:00', 90, 70, 83.00, 50, 30.0, 30, 100, NULL, 50.0, 120, '', '2024-11-19 22:07:03'),
+(30, 47, '2024-11-19', '17:10:00', 120, 80, 83.00, 50, 35.0, 30, 100, NULL, 50.0, 120, '', '2024-11-19 22:11:04'),
+(31, 47, '2024-11-19', '17:12:00', 120, 80, 83.00, 58, 35.0, 50, 100, NULL, 50.0, 120, '', '2024-11-19 22:13:00'),
+(32, 47, '2024-09-03', '17:16:00', 120, 80, 83.00, 50, 30.0, 50, 100, NULL, 12.0, 120, '', '2024-11-19 22:28:14'),
+(33, 47, '2024-09-03', '18:09:00', 120, 80, 83.00, 80, 35.0, 30, 100, NULL, 20.0, 120, '', '2024-11-19 23:09:55'),
+(34, 54, '2024-09-15', '22:52:00', 120, 80, 83.00, 50, 35.0, 50, 100, NULL, 20.0, 120, '', '2024-11-20 03:53:08'),
+(35, 47, '2024-11-19', '17:16:00', 120, 80, 83.00, 50, 30.0, 50, 100, NULL, 20.0, 150, '', '2024-11-20 04:08:28'),
+(36, 47, '2024-10-15', '17:54:00', 120, 80, 83.00, 52, 35.0, 35, 100, NULL, 20.0, 150, '', '2024-11-20 22:55:05'),
+(37, 47, '2024-10-01', '17:55:00', 110, 80, 76.00, 40, 35.0, 30, 80, NULL, 20.0, 120, '', '2024-11-20 22:55:54'),
+(38, 47, '2024-09-24', '22:56:00', 100, 50, 68.00, 40, 35.0, 50, 100, NULL, 20.0, 120, '', '2024-11-20 22:57:15'),
+(39, 47, '2024-09-15', '17:57:00', 90, 70, 62.00, 40, 35.0, 50, 85, NULL, 20.0, 120, '', '2024-11-20 22:57:40'),
+(40, 47, '2024-09-03', '21:00:00', 120, 80, 83.00, 50, 34.0, 50, 100, NULL, 20.0, 120, '', '2024-11-20 22:58:06'),
+(41, 47, '2024-10-02', '21:58:00', 120, 88, 83.00, 50, 35.0, 50, 100, NULL, 20.0, 120, '', '2024-11-20 22:58:32'),
+(42, 47, '2024-09-15', '23:03:00', 90, 55, 62.00, 50, 35.0, 50, 100, NULL, 20.0, 120, '', '2024-11-20 22:59:18'),
+(43, 47, '2024-09-26', '22:59:00', 120, 80, 83.00, 50, 35.0, 50, 100, NULL, 20.0, 120, '', '2024-11-20 22:59:51'),
+(44, 47, '2024-09-15', '18:00:00', 120, 80, 83.00, 50, 35.0, 25, 100, NULL, 20.0, 150, '', '2024-11-20 23:00:49'),
+(45, 47, '2024-09-03', '22:02:00', 120, 80, 83.00, 50, 25.0, 50, 100, NULL, 20.0, 120, '', '2024-11-20 23:02:29'),
+(46, 54, '2024-11-21', '17:31:00', 120, 80, 83.00, 52, 35.0, 52, 100, 20.000, NULL, 120, '', '2024-11-21 22:31:17'),
+(47, 54, '2024-11-21', '17:40:00', 120, 80, 83.00, 50, 35.0, 50, 100, NULL, 20.0, 20, '', '2024-11-21 22:40:47');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `role` enum('jefe','staff','user') COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `reset_token` varchar(64) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `reset_token_expiration` datetime DEFAULT NULL,
+  `is_active` tinyint(1) DEFAULT '1',
+  `profile_image` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `numero_identificacion` varchar(20) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `role`, `created_at`, `reset_token`, `reset_token_expiration`, `is_active`, `profile_image`, `numero_identificacion`) VALUES
+(23, 'Alejandra26', '$2a$10$7Fdcdtgi14GCxSlMtZbrG.HbmK64PD8X/PGzjlZ3d.y.2nJcxa7yK', 'alejandra.hdez.2616@gmial.com', 'jefe', '2024-11-16 17:41:07', NULL, NULL, 1, 'profile-1731778867152-593037909.jpg', '1091967574'),
+(24, 'laura quintero', '$2a$10$4QjKIOQOj3a8.raQBl.3FO.JcIcrmp7em6L1NqOkPOk3sZQEg8Fde', 'lauraquintero881120@gmail.com', 'jefe', '2024-11-16 19:33:09', 'bf8d8b908a3f116abad05b935b6e2f0286db3eb204e034185ffc84059d3a1b22', '2024-11-18 12:39:58', 1, NULL, '1005542019'),
+(28, 'Laura Solares', '$2a$10$fOX4eqp2J5Xg2awPrgEWFeL3oAIPzy3LFEbYp.ZtwwHybim0Siwsm', 'lsolares@unab.edu.co', 'user', '2024-11-18 19:20:40', NULL, NULL, 1, NULL, '10967574569'),
+(29, 'qwe', '$2a$10$AKv8alf6bnJ9gVB7Vu8yBe1tsCijcX2xzrUnb4HnUL2XnIc66Goiu', 'sdasdwef8435@gmail.com', 'user', '2024-11-20 03:49:27', NULL, NULL, 1, 'profile-1732074566900-928359821.png', '123456789'),
+(30, 'qeqweqweas', '$2a$10$h6SKknz5xLobXQitbrehi.DekqGFsQ.mOTAYtAMDshd/Oz5hqNSl2', 'asdasfwfsdc541@gmail.com', 'user', '2024-11-20 04:22:31', NULL, NULL, 1, 'profile-1732076551197-224064163.png', '451245312');
+
 --
 -- Índices para tablas volcadas
 --
@@ -136,6 +233,22 @@ ALTER TABLE `patients`
   ADD UNIQUE KEY `numero_identificacion` (`numero_identificacion`);
 
 --
+-- Indices de la tabla `registros_paciente`
+--
+ALTER TABLE `registros_paciente`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `patient_id` (`id_paciente`) USING BTREE;
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `nombreU` (`username`) USING BTREE,
+  ADD UNIQUE KEY `numero_identificacion` (`numero_identificacion`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -144,6 +257,28 @@ ALTER TABLE `patients`
 --
 ALTER TABLE `patients`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1079053;
+
+--
+-- AUTO_INCREMENT de la tabla `registros_paciente`
+--
+ALTER TABLE `registros_paciente`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `registros_paciente`
+--
+ALTER TABLE `registros_paciente`
+  ADD CONSTRAINT `registros_paciente_ibfk_1` FOREIGN KEY (`id_paciente`) REFERENCES `patients` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
